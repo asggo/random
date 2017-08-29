07785cac1da7a42e50263d2a607452834391653c
averagesecurityguy <stephen@averagesecurityguy.info>
2017-08-28 23:25:18 -0400

    Refactor and add tests.

--- random.go
+++ random.go
@@ -1,126 +1,153 @@
 // The random package provides cryptographically secure random integers,
-// strings, and 
+// strings, and
 package random
 
 import (
-    "errors"
-    "math"
-    "crypto/rand"
+	"crypto/rand"
+	"errors"
+	"math"
 )
 
+// Bytes returns an array of the specified size filled with random bytes.
+func Bytes(size int) []byte {
+	array := make([]byte, size)
+	_, err := rand.Read(array)
 
-// RandomBytes returns an array of the specified size filled with random bytes.
-func RandomBytes(size int) ([]byte, error) {
-    array := make([]byte, size)
-    _, err := rand.Read(array)
+	if err != nil {
+		panic(err)
+	}
 
-    if err != nil {
-        return array, err
-    }
-
-	return array, nil
+	return array
 }
 
-
 // BytesToInt returns a uint64 created from an array of 1 to 8 bytes.
-func bytesToInt(array []byte) (uint64, error) {
-	if len(array) == 0 || len(array) > 8 {
-        return uint64(0), errors.New("Need 1 to 8 bytes to create integer.")
-    }
+func bytesToInt(array []byte) uint64 {
+	var integer uint64
+	size := len(array)
 
-    var integer uint64
-    size := len(array)
-
-    for i, b := range array {
+	for i, b := range array {
 		shift := uint64((size - i - 1) * 8)
 		integer |= uint64(b) << shift
 	}
 
-    return integer, nil
+	return integer
 }
 
+// Uint64Range returns a random 64-bit unsigned integer in the range [start, end)
+func Uint64Range(start, end uint64) (uint64, error) {
+	val := uint64(0)
+
+	if start >= end {
+		return val, errors.New("Start value must be less than end value.")
+	}
+
+	for {
+		val = bytesToInt(Bytes(8))
+		if val <= (math.MaxUint64 - (math.MaxUint64 % end)) {
+			break
+		}
+	}
+
+	val = val % (end - start)
+	val = val + start
+
+	return val, nil
+}
+
+// Chars returns a random string using the given character set
+func Chars(charset string, n int) string {
+	if n == 0 {
+		return ""
+	}
+
+	if len(charset) == 0 {
+		return ""
+	}
+
+	length := uint64(len(charset))
+	b := make([]byte, n)
+
+	for i := range b {
+		b[i] = charset[Uint64n(length)]
+	}
+
+	return string(b)
+}
 
 // Uint8 returns a random 8-bit unsigned integer.
 func Uint8() uint8 {
-    b, err := RandomBytes(1)
-    if err != nil {
-        return val, err
-    }
+	i, _ := Uint64Range(0, math.MaxUint8)
 
-    return uint8(b[0])
+	return uint8(i)
+}
+
+// Int8 returns a random 8-bit signed integer.
+func Int8() int8 {
+	i, _ := Uint64Range(0, math.MaxUint8)
+
+	return int8(i)
 }
 
 // Uint16 returns a random 16-bit unsigned integer.
 func Uint16() uint16 {
-    var val uint64
+	i, _ := Uint64Range(0, math.MaxUint16)
 
-    b, err := RandomBytes(2)
-    if err != nil {
-        return val, err
-    }
+	return uint16(i)
+}
 
-    val, err = bytesToInt(b)
-    if err != nil {
-        return val, err
-    }
+// Int16 returns a random 16-bit signed integer.
+func Int16() int16 {
+	i, _ := Uint64Range(0, math.MaxUint16)
 
-    return uint16(val)
+	return int16(i)
 }
 
 // Uint32 returns a random 32-bit unsigned integer.
 func Uint32() uint32 {
-    b, err := RandomBytes(4)
-    if err != nil {
-        return val, errors.New("Could not read random bytes from OS.")
-    }
+	i, _ := Uint64Range(0, math.MaxUint32)
 
-    val, err = bytesToInt(b)
-    if err != nil {
-        return val, errors.New("Could not convert bytes to unsigned integer.")
-    }
+	return uint32(i)
+}
 
-    return uint32(val)
+// Int32 returns a random 32-bit signed integer.
+func Int32() int32 {
+	i, _ := Uint64Range(0, math.MaxUint32)
+
+	return int32(i)
 }
 
 // Uint64 returns a random 64-bit unsigned integer.
 func Uint64() uint64 {
-    b, err := RandomBytes(2)
-    if err != nil {
-        return val, errors.New("Could not read random bytes from OS.")
-    }
+	i, _ := Uint64Range(0, math.MaxUint64)
 
-    val, err = bytesToInt(b)
-    if err != nil {
-        return val, errors.New("Could not convert bytes to unsigned integer.")
-    }
-
-    return val
+	return i
 }
 
-// Return a random uint64 in the range [start, end)
-func Uint64Range(start, end uint64) (uint64, error) {
-	val = uint64(0)
+// Int64 returns a random 64-bit signed integer.
+func Int64() int64 {
+	i, _ := Uint64Range(0, math.MaxUint64)
 
-    if start >= end {
-        return val, errors.New("Start value must be less than end value.")
-    }
+	return int64(i)
+}
 
-    for {
-		b, err := RandomBytes(8)
-		if err != nil {
-			return val, errors.New("Could not read random bytes from OS.")
+// Uint64n returns a random 64-bit unsigned integer in the range [0, n)
+func Uint64n(n uint64) uint64 {
+	i, _ := Uint64Range(0, n)
+
+	return i
+}
+
+// Int64n returns a random 64-bit signed integer in the range [0, n)
+func Int64n(n uint64) int64 {
+	i, _ := Uint64Range(0, n)
+
+	for {
+		v := int64(i)
+
+		if v >= 0 {
+			return v
 		}
 
-        val, err = bytesToInt(b)
-		if err != nil {
-			return val, errors.New("Could not convert bytes to unsigned integer.")
-		}
-
-        if (val <= (math.MaxUint64 - (math.MaxUint64 % end))){ break }
-    }
-
-    val = val % (end - start)
-    val = val + start
-
-    return val, nil
+		i, _ = Uint64Range(0, n)
+	}
 }
--- random_test.go
+++ random_test.go
@@ -1,76 +1,182 @@
-package cryptanalysis
+package random
 
 import (
+	"math"
+	"strings"
 	"testing"
 )
 
-type byteint struct{
+type char struct {
+	charset string
+	length  int
+}
+
+type byteint struct {
 	bytearray []byte
 	result    uint64
 }
 
-type intrange struct{
+type intrange struct {
 	start uint64
 	end   uint64
 }
 
 func TestRandomBytes(t *testing.T) {
-	var tests = []int{1, 5, 10,}
+	var tests = []int{1, 5, 10}
 
 	for _, test := range tests {
-		r, _ := RandomBytes(test)
+		r := Bytes(test)
 		if len(r) != test {
 			t.Error("Expected", test, "got", len(r))
 		}
 	}
 }
 
-
-func TestBytesToInt(t *testing.T) {
+func TestbytesToInt(t *testing.T) {
 	var tests = []byteint{
-		{[]byte{128,0,0,0,0,0,0,0}, 9223372036854775808},
-		{[]byte{0,0,0,0,128,0,0,0}, 2147483648},
-		{[]byte{0,0,0,0,0,0,0,1}, 1},
-		{[]byte{0,0,0,0,0,3}, 3},
-		{[]byte{0,0,0,5}, 5},
-
-
-	}
-
-	_, err := BytesToInt([]byte{})
-	if err == nil {
-		t.Error("Empty byte array should produce an error.")
-	}
-
-	_, err = BytesToInt([]byte{0,0,0,0,0,0,0,0,0})
-	if err == nil {
-		t.Error("Long byte array should produce an error.")
+		{[]byte{128, 0, 0, 0, 0, 0, 0, 0}, 9223372036854775808},
+		{[]byte{0, 0, 0, 0, 128, 0, 0, 0}, 2147483648},
+		{[]byte{0, 0, 0, 0, 0, 0, 0, 1}, 1},
+		{[]byte{0, 0, 0, 0, 0, 3}, 3},
+		{[]byte{0, 0, 0, 5}, 5},
 	}
 
 	for _, test := range tests {
-		i, _ := BytesToInt(test.bytearray)
+		i := bytesToInt(test.bytearray)
 		if i != test.result {
 			t.Error("Expected", test.result, "got", i)
 		}
 	}
 }
 
-
-func TestRandomIntRange(t *testing.T) {
+func TestUint64Range(t *testing.T) {
 	var tests = []intrange{
 		{0, 10},
 		{9223372036854775800, 9223372036854775808},
 	}
 
-	_, err := RandomIntRange(10, 0)
+	_, err := Uint64Range(10, 0)
 	if err == nil {
 		t.Error("Start greater than end should produce an error.")
 	}
 
 	for _, test := range tests {
-		i, _:= RandomIntRange(test.start, test.end)
+		i, _ := Uint64Range(test.start, test.end)
 		if i < test.start || i > test.end {
 			t.Error("Expected integer within range of", test.start, "and", test.end, "got", i)
+		}
+	}
+}
+
+func TestChars(t *testing.T) {
+	var chars = []char{
+		{"abcdefghijklmnopqrstuvwxyz", 5},
+		{"ABCDEFGHIJKLMNOPQRSTUVWXYZ", 20},
+		{"0123456789abcdef", 15},
+	}
+
+	s := Chars("", 15)
+	if s != "" {
+		t.Error("An empty character set should produce an empty string.")
+	}
+
+	s = Chars("ABCDEF", 0)
+	if s != "" {
+		t.Error("N <= 0 should produce an empty string.")
+	}
+
+	for _, char := range chars {
+		s := Chars(char.charset, char.length)
+		if len(s) != char.length {
+			t.Error("Expected string of length", char.length, "and got string of length", len(s))
+		}
+
+		for _, c := range s {
+			if !strings.Contains(char.charset, string(c)) {
+				t.Error("Expected a character in", char.charset, "got", c)
+			}
+		}
+	}
+}
+
+func TestUint8(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		n := Uint8()
+		if n < 0 || n > math.MaxUint8 {
+			t.Error("Expected integer within range of", 0, "and", math.MaxUint8-1, "got", n)
+		}
+	}
+}
+
+func TestInt8(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		n := Int8()
+		if n < math.MinInt8 || n > math.MaxInt8 {
+			t.Error("Expected integer within range of", math.MinInt8, "and", math.MaxInt8, "got", n)
+		}
+	}
+}
+
+func TestUint16(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		n := Uint16()
+		if n < 0 || n > math.MaxUint16 {
+			t.Error("Expected integer within range of", 0, "and", math.MaxUint16, "got", n)
+		}
+	}
+}
+
+func TestInt16(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		n := Int16()
+		if n < math.MinInt16 || n > math.MaxInt16 {
+			t.Error("Expected integer within range of", math.MinInt16, "and", math.MaxInt16, "got", n)
+		}
+	}
+}
+
+func TestUint32(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		n := Uint32()
+		if n < 0 || n > math.MaxUint32 {
+			t.Error("Expected integer within range of", 0, "and", math.MaxUint32, "got", n)
+		}
+	}
+}
+
+func TestInt32(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		n := Int32()
+		if n < math.MinInt32 || n > math.MaxInt32 {
+			t.Error("Expected integer within range of", math.MinInt32, "and", math.MaxInt32, "got", n)
+		}
+	}
+}
+
+func TestUint64(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		n := Uint64()
+		if n < 0 || n > math.MaxUint64 {
+			t.Error("Expected integer within range of", 0, "and 18446744073709551615 got", n)
+		}
+	}
+}
+
+func TestInt64(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		n := Int64()
+		if n < math.MinInt64 || n > math.MaxInt64 {
+			t.Error("Expected integer within range of", math.MinInt64, "and", math.MaxInt64, "got", n)
+		}
+	}
+}
+
+func TestUint64n(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		max := uint64(i * i)
+		n := Uint64n(max)
+		if n < 0 || n > uint64(max) {
+			t.Error("Expected integer within range of", 0, "and", max, "got", n)
 		}
 	}
 }
